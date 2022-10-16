6fcb63ed31e6ed4c93c7804ecf9dba32c8bb2363
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-16 11:47:33 +0600

    Lecture1 done

--- lecture1/Program.cs
+++ lecture1/Program.cs
@@ -1,1 +1,49 @@
-﻿
+﻿string NumbersFor (int a, int b)
+{
+    string result = string.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumbersRec (int a, int b)
+{
+    if (a < b)
+    {
+        return $"{a} " + NumbersRec(a + 1, b);
+    }
+    else
+    {
+        return $"{b}";
+    }
+}
+
+Console.WriteLine(NumbersFor(1, 10));
+Console.WriteLine(NumbersRec(1, 10));
+
+string NumbersForNew (int a, int b)
+{
+    string result = string.Empty;
+    for (int i = b; i >= a; i--)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumbersRecNew (int a, int b)
+{
+    if (a < b - 1)
+    {
+        return NumbersRec(a + 1, b) + $"{b} ";
+    }
+    else
+    {
+        return string.Empty;
+    }
+}
+
+Console.WriteLine(NumbersForNew(1, 10));
+Console.WriteLine(NumbersRecNew(1, 10));
