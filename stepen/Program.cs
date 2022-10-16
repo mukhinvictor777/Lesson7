9e28d374060c8d469f539eb181aa12595eb74380
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-16 14:38:54 +0600

    Сделал степень

--- stepen/Program.cs
+++ stepen/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿int StepenFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
+
+int StepenRec(int a, int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+        return StepenRec(a, n - 1) * a;
+}
+
+Console.WriteLine(StepenFor(2, 10));
+Console.WriteLine(StepenRec(2, 10));
