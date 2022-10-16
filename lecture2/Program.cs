6e09b6bb5178598ed82d9064279bbe22c99b8486
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-16 14:29:10 +0600

    Решил факториал и начал степень

--- lecture2/Program.cs
+++ lecture2/Program.cs
@@ -22,3 +22,26 @@ int SumRec (int n)
 
 Console.WriteLine(SumFor(10));
 Console.WriteLine(SumRec(10));
+
+int FactorialFor(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+int FactorialRec(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+        return n * FactorialRec(n - 1);
+}
+
+Console.WriteLine(FactorialFor(10));
+Console.WriteLine(FactorialRec(10));
