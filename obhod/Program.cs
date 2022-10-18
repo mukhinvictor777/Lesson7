fddb5ccdafe0dec6d47c94e14d0e7931eb7d6cea
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-18 14:24:50 +0600

    обход в процессе, затем тест эффективности

---
+++ obhod/Program.cs
@@ -0,0 +1,9 @@
+﻿string path = "/Users/mukhi/Desktop/Geek Brains/Знакомство с С#/Practice6";
+DirectoryInfo d1 = new DirectoryInfo(path);
+System.Console.WriteLine(d1.CreationTime);
+FileInfo[] fi = d1.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
