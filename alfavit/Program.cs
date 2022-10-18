04db51b6f39605b1f787033689c744c444304b42
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-18 12:48:12 +0600

    Алфавит

---
+++ alfavit/Program.cs
@@ -0,0 +1,26 @@
+﻿char[] s = {'а', 'и', 'с', 'в'};
+int count = s.Length;
+int n = 1;
+
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine($"{n++, -5} {s[i]}");
+}
+Console.WriteLine();
+
+
+void FindWords(string alphabet, char[] word, int Length = 0)
+{
+       if (Length == word.Length)
+    {
+        Console.WriteLine($"{new String(word)}");
+        return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[Length] = alphabet[i];
+        FindWords(alphabet, word, Length + 1);
+    }
+}
+
+FindWords ("аисв", new char[2]);
