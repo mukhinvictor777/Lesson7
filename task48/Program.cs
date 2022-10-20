81146ee21d472b9d61af7bbe4f95ecef004b145e
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-20 23:14:14 +0600

    task48 done

--- task48/Program.cs
+++ task48/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿int[,] generateMNArray(int m, int n)
+{
+    int[,] twoDArray = new int[m, n];
+    for (int i = 0; i < twoDArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < twoDArray.GetLength(1); j++)
+        {
+            twoDArray[i, j] = i + j;
+        }
+    }
+    return twoDArray;
+}
+
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+
+}
+
+void print2DArray(int[,] arrayToPrint)
+{
+    Console.Write("\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        printColorData(i + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        printColorData(i + "\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+           Console.Write(arrayToPrint[i, j] + "\t"); 
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] generatedArray = generateMNArray(3, 4);
+print2DArray(generatedArray);
