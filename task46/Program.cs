2164689874a9dd6cab68641bdded2e8d4d58be48
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-20 21:48:12 +0600

    task46 done

--- task46/Program.cs
+++ task46/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] generate2DArray(int hight, int wight, int randomStart, int randomEnd)
+{
+    int[,] twoDArray = new int[hight, wight];
+    for (int i = 0; i < twoDArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < twoDArray.GetLength(1); j++)
+        {
+            twoDArray[i, j] = new Random().Next(randomStart, randomEnd + 1);
+        }
+    }
+    return twoDArray;
+}
+
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+
+}
+
+void print2DArray(int[,] arrayToPrint)
+{
+    Console.Write("\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        printColorData(i+1 + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        printColorData(i+1 + "\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+           Console.Write(arrayToPrint[i, j] + "\t"); 
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] generatedArray = generate2DArray(10, 10, -999, 999);
+print2DArray(generatedArray);
